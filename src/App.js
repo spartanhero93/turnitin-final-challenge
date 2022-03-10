9f03605d0a054bb15b0ef169e40b13433239aae8
spartanhero93 <spartanhero93@gmail.com>
2022-03-10 17:23:02 -0500

    completed fetch of multiple dog links

--- src/App.js
+++ src/App.js
@@ -15,11 +15,14 @@ function App() {
     for(let i = 0; i < 10; i++){
       promises.push(await fetch('https://dog.ceo/api/breeds/image/random'))
     }
-    const links = await Promise.all(promises.map(request => request.json()))
+    // create an array of promises to be resolved
+    const links = await Promise.all(
+      promises.map(request => request.json())
+    )
+    // Map through each item, and add a breed prop, swap the - for spaces
+    links.map(i => i.breed = i.message.split('/')[4].replace('-', ' '))
+    console.log(links)
     getDogLinks(links)
-  }
-  function getBreedInfo(arrayOfLinks){
-    
   }
   return (
     <div className="App">
@@ -30,7 +33,7 @@ function App() {
       
         {dogImg.status === 'success' ? 
           <div>
-            <img src={dogImg.message} alt='dogs.message'></img>
+            <img src={dogImg.message} alt={dogImg.message}></img>
             <h3>Cute Doggo</h3>
           </div>
           : <h2>Loading....</h2>
@@ -38,7 +41,11 @@ function App() {
         <div>
           {dogLinks.length ? 
           <ul>
-            {dogLinks.map(i => <li><a href={i.message}>test</a></li>)}
+            {dogLinks.map(i => 
+              <li key={i.breed}>
+                <a href={i.message}>{i.message}</a>
+                <p>{i.breed}</p>
+              </li>)}
           </ul>  
           : ''
         }
