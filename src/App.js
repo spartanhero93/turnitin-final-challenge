6fff3babd38683b34b90f14f6956946602b8465a
spartanhero93 <spartanhero93@gmail.com>
2022-03-10 11:58:06 -0500

    inital fetch completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import logo from './logo.svg';
+import {useState} from 'react'
 import './App.css';
 
 function App() {
+  const [dogs, getDogs] = useState({})
+
+  async function fetchDogImg() {
+    const res = await fetch('https://dog.ceo/api/breeds/image/random')
+    const data = await res.json()
+    getDogs(data)
+  }
   return (
     <div className="App">
-      
+      <h1>Hello</h1>
+      <button onClick={() => fetchDogImg()}>Fetch that data</button>
+      <button onClick={() => console.log(dogs)}>log the state</button>
+
+      <div>
+        {dogs.status === 'success' ? 
+          <div>
+            <img src={dogs.message} alt='dogs.message'></img>
+            <h3>Cute Doggo</h3>
+          </div>
+          : <h2>Loading....</h2>
+        }
+      </div>
     </div>
   );
 }
